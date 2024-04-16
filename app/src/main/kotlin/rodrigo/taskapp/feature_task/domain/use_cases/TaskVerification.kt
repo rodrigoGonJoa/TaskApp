7436094ba0518a2a62df21c5e8340e8c0ccc532d
Rodrigo González <rodrigogonzalezjoaquin@gmail.com>
2024-04-16 21:40:04 +0200

    :necktie:  TaskVerification modified

--- app/src/main/kotlin/rodrigo/taskapp/feature_task/domain/use_cases/TaskVerification.kt
+++ app/src/main/kotlin/rodrigo/taskapp/feature_task/domain/use_cases/TaskVerification.kt
@@ -7,8 +7,38 @@ import rodrigo.taskapp.feature_task.domain.TaskError
 
 class TaskVerification {
     operator fun invoke(task: Task): Result<Unit, Error> {
-        if(task.content.length >= 1023){
+        val verifications = listOf(::contentLenght, ::isCompleted, ::scheduledTime)
+        return executeVerifications(verifications, task)
+    }
+
+    private fun executeVerifications(
+        verifications: List<(Task) -> Result<Unit, Error>>,
+        task: Task
+    ): Result<Unit, Error> = verifications.find {verification ->
+        verification(task) is Result.Error
+    }?.invoke(task) ?: Result.Success(Unit)
+
+
+    private fun contentLenght(task: Task): Result<Unit, Error> {
+        if (task.content.length >= 1023) {
             return Result.Error(TaskError.ContentTooLong)
+        }
+        return Result.Success(Unit)
+    }
+
+    private fun isCompleted(task: Task): Result<Unit, Error> {
+        if (task.isCompleted != null && task.completedAt == null) {
+            return Result.Error(TaskError.DateTimeCompletionIsNull)
+        }
+        if (task.isCompleted == null && task.completedAt != null) {
+            return Result.Error(TaskError.CompletionFlagIsNull)
+        }
+        return Result.Success(Unit)
+    }
+
+    private fun scheduledTime(task: Task): Result<Unit, Error> {
+        if (task.scheduledDate == null && task.scheduledTime != null) {
+            return Result.Error(TaskError.TimeSetWithoutDate)
         }
         return Result.Success(Unit)
     }
